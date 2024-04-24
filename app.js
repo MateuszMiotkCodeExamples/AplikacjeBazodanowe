c88a2c894a63e0671908ce73fb5189cb42555d01
Mateusz Miotk <mateusz.miotk@ug.edu.pl>
2024-04-24 11:58:07 +0200

    Add knex.js example version 2

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const app = express();
 const knex = require('knex')(require('./knexfile').development);
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'verySecretValue',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Ustaw na `true` jeśli używasz HTTPS
+}));
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true })); // Middleware do parsowania body formularza
 
 app.get('/', async (req, res) => {
-    const users = await knex('users').select('*');
-    res.render('index', { users });
+    let userName = 'Nieznajomy'; // Domyślna wartość, jeśli nie ma sesji ani ciasteczka
+    if (req.session.user) {
+        userName = req.session.user; // Użyj nazwy z sesji, jeśli dostępna
+    } else if (req.cookies.username) {
+        userName = req.cookies.username; // Użyj nazwy z ciasteczka, jeśli sesja nie istnieje
+        req.session.user = userName; // Odtwórz sesję z ciasteczka
+    }
+    try {
+        const users = await knex('users').select('*'); // Pobieranie użytkowników z bazy danych
+        res.render('index', { users, userName }); // Przekazanie użytkowników i nazwy użytkownika do szablonu
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Błąd serwera');
+    }
 });
 
 app.post('/users', async (req, res) => {
@@ -20,6 +42,30 @@ app.post('/users', async (req, res) => {
         console.error(error);
         res.status(500).send('Error adding user');
     }
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');  // Wyświetl formularz logowania
+});
+
+
+app.post('/login', (req, res) => {
+    const { username } = req.body;
+    req.session.user = username; // Przechowaj nazwę użytkownika w sesji
+    res.cookie('username', username, { maxAge: 900000, httpOnly: true }); // Ustaw ciasteczko
+    res.redirect('/');
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            console.error('Błąd podczas niszczenia sesji', err);
+            return res.status(500).send('Nie udało się wylogować');
+        }
+        res.clearCookie('username'); // Usuń ciasteczko z nazwą użytkownika
+        res.clearCookie('connect.sid'); // Usuń ciasteczko sesji
+        res.redirect('/');
+    });
 });
 
 
